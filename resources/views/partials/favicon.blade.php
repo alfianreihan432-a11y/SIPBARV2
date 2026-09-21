f180e36433be8c3bc5085f30d2e47cc878a3b81f
Salman <fatirpc9@gmail.com>
2026-09-21 10:43:51 +0700

    fix Deploy 2

--- resources/views/partials/favicon.blade.php
+++ resources/views/partials/favicon.blade.php
@@ -1,12 +1,9 @@
-{{-- SIPBAR Favicon Meta Tags (Dynamic + Multi-size Fallback) --}}
-{{-- v3: Regenerated with letterbox (proportional fit) from 375x666 source --}}
-@php
-    $favIconDynamic = $siteLogoLanding ?? (\App\Models\SiteSetting::get('site_logo_landing') ?: \App\Models\SiteSetting::get('site_logo', asset('favicon.ico')));
-@endphp
-<link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}?v=3">
-<link rel="shortcut icon" href="{{ asset('favicon.ico') }}?v=3">
-<link rel="icon" type="image/png" sizes="48x48" href="{{ asset('favicon-48x48.png') }}?v=3">
-<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}?v=3">
-<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}?v=3">
-<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}?v=3">
-<link rel="manifest" href="{{ asset('site.webmanifest') }}?v=3">
+{{-- SIPBAR Favicon Meta Tags (Multi-size) --}}
+{{-- v5: Transparent background, logo S+kubus biru gradasi --}}
+<link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}?v=5">
+<link rel="shortcut icon" href="{{ asset('favicon.ico') }}?v=5">
+<link rel="icon" type="image/png" sizes="48x48" href="{{ asset('favicon-48x48.png') }}?v=5">
+<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}?v=5">
+<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}?v=5">
+<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}?v=5">
+<link rel="manifest" href="{{ asset('site.webmanifest') }}?v=5">
